9cc7e2128e5b0a1400797572180b4474f9515442
dpi <pro@danielph.in>
2016-09-20 16:34:30 +0800

    Init

---
+++ README.md
@@ -0,0 +1,28 @@
+Integrates Twilio with SMS Framework.
+
+Copyright (C) 2016 Daniel Phin (@dpi)
+
+# License
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Installation
+
+ 1. Install module as per [standard procedure][drupal-module-install].
+ 2. This module has composer dependencies, see [instructions][composer-dependencies]
+    for information on how to resolve these dependencies.
+
+[drupal-module-install]: https://www.drupal.org/docs/8/extending-drupal/installing-contributed-modules "Installing Contributed Modules"
+[composer-dependencies]: https://www.drupal.org/docs/8/extending-drupal/installing-modules-composer-dependencies "Installing modules' Composer dependencies"
---
+++ src/Plugin/SmsGateway/Twilio.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Drupal\sms_twilio\Plugin\SmsGateway;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\sms\Message\SmsDeliveryReport;
+use Drupal\sms\Message\SmsMessageResultStatus;
+use Drupal\sms\Message\SmsMessageReportStatus;
+use Drupal\sms\Plugin\SmsGatewayPluginBase;
+use Drupal\sms\Message\SmsMessageInterface;
+use Drupal\sms\Message\SmsMessageResult;
+use Twilio\Exceptions\RestException;
+use Twilio\Rest\Client;
+
+/**
+ * @SmsGateway(
+ *   id = "twilio",
+ *   label = @Translation("Twilio"),
+ *   outgoing_message_max_recipients = 1,
+ *   reports_push = TRUE,
+ * )
+ */
+class Twilio extends SmsGatewayPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'account_sid' => '',
+      'auth_token' => '',
+      'from' => '',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+
+    $form['twilio'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Twilio'),
+      '#open' => TRUE,
+    ];
+
+    $form['twilio']['help'] = [
+      '#type' => 'html_tag',
+      '#tag' => 'p',
+      '#value' => $this->t('API keys can be found at <a href="https://www.twilio.com/console">https://www.twilio.com/console</a>.'),
+    ];
+
+    $form['twilio']['account_sid'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Account SID'),
+      '#default_value' => $config['account_sid'],
+      '#placeholder' => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+      '#required' => TRUE,
+    ];
+
+    $form['twilio']['auth_token'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Auth token'),
+      '#default_value' => $config['auth_token'],
+      '#placeholder' => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+      '#required' => TRUE,
+    ];
+
+    $form['twilio']['from'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('From number'),
+      '#default_value' => $config['from'],
+      '#required' => TRUE,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $this->configuration['account_sid'] = trim($form_state->getValue('account_sid'));
+    $this->configuration['auth_token'] = trim($form_state->getValue('auth_token'));
+    $this->configuration['from'] = $form_state->getValue('from');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function send(SmsMessageInterface $sms_message) {
+    // Messages: https://www.twilio.com/docs/api/rest/message
+    // Testing API: https://www.twilio.com/docs/api/rest/test-credentials
+
+    $recipient = $sms_message->getRecipients()[0];
+    $result = new SmsMessageResult();
+
+    $account_sid = $this->configuration['account_sid'];
+    $auth_token = $this->configuration['auth_token'];
+
+    $client = new Client($account_sid, $auth_token);
+    $options = [
+      'from' => $this->configuration['from'],
+      'body' => $sms_message->getMessage(),
+    ];
+
+    $report = new SmsDeliveryReport();
+    try {
+      $message = $client->messages->create($recipient, $options);
+      $report->setStatus(SmsMessageReportStatus::QUEUED);
+      $report->setMessageId($message->uri);
+    }
+    catch (RestException $e) {
+      $code = $e->getCode();
+      $message = $e->getMessage();
+
+      if (in_array($code, [21211, 21612, 21610, 21614])) {
+        // 21211: Recipient is invalid. (Test recipient: +15005550001)
+        // 21612: Cannot route to this recipient. (Test recipient: +15005550002)
+        // 21610: Recipient is blacklisted. (Test recipient: +15005550004)
+        // 21614: Recipient is incapable of receiving SMS.
+        //       (Test recipient: +15005550009)
+        $report->setStatus(SmsMessageReportStatus::INVALID_RECIPIENT);
+        $report->setStatusMessage($message);
+      }
+      elseif ($code == 21408) {
+        // 21408: Account doesn't have the international permission.
+        //       (Test recipient: +15005550003)
+        $result->setError(SmsMessageResultStatus::ACCOUNT_ERROR);
+        $result->setStatusMessage($message);
+      }
+      else {
+        $report->setStatus(SmsMessageReportStatus::ERROR);
+        $report->setStatusMessage($message);
+      }
+    }
+
+    if ($report->getStatus()) {
+      $result->setReports([$report]);
+    }
+
+    return $result;
+  }
+
+}
